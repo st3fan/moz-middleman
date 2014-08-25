48d33831d07808d489a8c1e5ae26f72dbabcf54c
Stefan Arentz <stefan@arentz.ca>
2014-08-25 13:02:41 -0400

    Adding FxA support in the task executor

--- sources/middleman/tasks/__init__.py
+++ sources/middleman/tasks/__init__.py
@@ -10,6 +10,7 @@ from redis import StrictRedis
 import selenium
 
 from middleman.methods.persona import login as persona_login
+from middleman.methods.fxa import login as fxa_login
 
 
 redis = StrictRedis(host='localhost', port=6379, db=0)
@@ -26,13 +27,27 @@ def parse_cookies(header):
     return cookies
 
 
-def broker_persona_session(session):
+def broker_persona_session(config):
     try:
         driver = selenium.webdriver.Remote(
             desired_capabilities=selenium.webdriver.DesiredCapabilities.FIREFOX,
             command_executor="http://127.0.0.1:4444/wd/hub")
-        return persona_login(driver, session["config"])
+        return persona_login(driver, config)
     except Exception as e:
+        print "Failed to broker persona session", str(e)
+        return None
+    finally:
+        driver.quit()
+
+
+def broker_fxa_session(config):
+    try:
+        driver = selenium.webdriver.Remote(
+            desired_capabilities=selenium.webdriver.DesiredCapabilities.FIREFOX,
+            command_executor="http://127.0.0.1:4444/wd/hub")
+        return fxa_login(driver, config)
+    except Exception as e:
+        drover.save_screenshot("/tmp/failure.png")
         print "Failed to broker persona session", str(e)
         return None
     finally:
@@ -48,17 +63,22 @@ def broker_session(session_id, config):
     config = session["config"]
 
     if session["config"]["method"] == "persona":
-        cookies = broker_persona_session(session)
-        if not cookies:
-            session["cookies"] = None
-            session["state"] = "FAILURE"
-            session["reason"] = "No cookies found"
-        else:
-            session["cookies"] = cookies
-            session["state"] = "FINISHED"
+        cookies = broker_persona_session(session["config"])
+    if session["config"]["method"] == "fxa":
+        cookies = broker_fxa_session(session["config"])
     else:
         session["state"] = "FAILURE"
         session["reason"] = "Unknown method '%s'" % session["config"]["method"]
+        redis.set("session:%s" % session_id, json.dumps(session))
+        return
+
+    if not cookies:
+        session["cookies"] = None
+        session["state"] = "FAILURE"
+        session["reason"] = "No cookies found"
+    else:
+        session["result"] = dict(cookies=cookies)
+        session["state"] = "SUCCESS"
 
     print "RESULT", json.dumps(session, indent=4)
-    redis.set("session:%s" % session_id, json.dumps(session))
+    redis.setex("session:%s" % session_id, 300, json.dumps(session))
